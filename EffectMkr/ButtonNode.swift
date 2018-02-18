1302150243a1e1f1b163466f95497aa272631db9
SolanaAlfredo <asv@SolanaAlfredos-MacBook-Pro.local>
2018-02-19 04:31:52 +0900

    Add button handler class ButtonNode

---
+++ EffectMkr/ButtonNode.swift
@@ -0,0 +1,127 @@
+//
+//  ButtonNode.swift
+//  EffectMkr
+//
+//  Created by SolanaAlfredo on 2/18/18.
+//  Copyright © 2018 SolanaAlfredo. All rights reserved.
+//
+
+import SpriteKit
+
+class ButtonNode: SKSpriteNode {
+
+    enum ButtonActionType: Int {
+        case TouchUpInside = 1,
+        TouchDown, TouchUp
+    }
+
+
+    var isEnabled: Bool = true {
+        didSet {
+            if (disabledTexture != nil) {
+                texture = isEnabled ? defaultTexture : disabledTexture
+            }
+        }
+    }
+    var isSelected: Bool = false {
+        didSet {
+            texture = isSelected ? selectedTexture : defaultTexture
+        }
+    }
+
+    var defaultTexture: SKTexture
+    var selectedTexture: SKTexture
+
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+
+    init(normalTexture defaultTexture: SKTexture!, selectedTexture:SKTexture!, disabledTexture: SKTexture?) {
+
+        self.defaultTexture = defaultTexture
+        self.selectedTexture = selectedTexture
+        self.disabledTexture = disabledTexture
+
+        super.init(texture: defaultTexture, color: UIColor.white, size: defaultTexture.size())
+        isUserInteractionEnabled = true
+
+        let bugFixLayerNode = SKSpriteNode(texture: nil, color: UIColor.clear, size: defaultTexture.size())
+        bugFixLayerNode.position = self.position
+        addChild(bugFixLayerNode)
+
+    }
+
+    func setButtonAction(target: AnyObject, triggerEvent event:ButtonActionType, action:Selector) {
+
+        switch (event) {
+        case .TouchUpInside:
+            targetTouchUpInside = target
+            actionTouchUpInside = action
+        case .TouchDown:
+            targetTouchDown = target
+            actionTouchDown = action
+        case .TouchUp:
+            targetTouchUp = target
+            actionTouchUp = action
+        }
+
+    }
+
+    var disabledTexture: SKTexture?
+    var actionTouchUpInside: Selector?
+    var actionTouchUp: Selector?
+    var actionTouchDown: Selector?
+    weak var targetTouchUpInside: AnyObject?
+    weak var targetTouchUp: AnyObject?
+    weak var targetTouchDown: AnyObject?
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (!isEnabled) {
+            return
+        }
+        isSelected = true
+        if (targetTouchDown != nil && targetTouchDown!.responds(to: actionTouchDown)) {
+            UIApplication.shared.sendAction(actionTouchDown!, to: targetTouchDown, from: self, for: nil)
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        if (!isEnabled) {
+            return
+        }
+
+        let touch: AnyObject! = touches.first
+        let touchLocation = touch.location(in: parent!)
+
+        if (frame.contains(touchLocation)) {
+            isSelected = true
+        } else {
+            isSelected = false
+        }
+
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (!isEnabled) {
+            return
+        }
+
+        isSelected = false
+
+        if (targetTouchUpInside != nil && targetTouchUpInside!.responds(to: actionTouchUpInside!)) {
+            let touch: AnyObject! = touches.first
+            let touchLocation = touch.location(in: parent!)
+
+            if (frame.contains(touchLocation) ) {
+                UIApplication.shared.sendAction(actionTouchUpInside!, to: targetTouchUpInside, from: self, for: nil)
+            }
+
+        }
+
+        if (targetTouchUp != nil && targetTouchUp!.responds(to: actionTouchUp!)) {
+            UIApplication.shared.sendAction(actionTouchUp!, to: targetTouchUp, from: self, for: nil)
+        }
+    }
+
+}
